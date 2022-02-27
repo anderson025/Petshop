113081ba0c145df0f0c8fe9edab03829a14594c2
Anderson Silva <sistemas.anderson@hotmail.com>
2022-02-27 17:26:27 -0300

    Tratando exceções e referências Cíclicas

--- src/main/java/com/anderson/petshop/service/CategoriaService.java
+++ src/main/java/com/anderson/petshop/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.anderson.petshop.domain.Categoria;
 import com.anderson.petshop.repository.CategoriaRepository;
+import com.anderson.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID:" + id + ", Tipo:" + Categoria.class.getName()));
 	}
 }
