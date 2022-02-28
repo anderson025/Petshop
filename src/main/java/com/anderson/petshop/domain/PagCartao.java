8bb4cd0ececf111be94e0d6d526e3dc8b369d593
Anderson Silva <sistemas.anderson@hotmail.com>
2022-02-28 17:12:56 -0300

    Criacao das classes, pagamento, cartao , dinheiro e servico e o relacionamento entre pagamento e servico

---
+++ src/main/java/com/anderson/petshop/domain/PagCartao.java
@@ -0,0 +1,34 @@
+package com.anderson.petshop.domain;
+
+import javax.persistence.Entity;
+
+import com.anderson.petshop.domain.enuns.SituacaoPagamento;
+
+@Entity
+public class PagCartao extends Pagamento {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer parcelas;
+	
+	public PagCartao() {
+		
+	}
+
+	public PagCartao(Integer id, Double valor, SituacaoPagamento situacao,Servico servico, Integer parcelas) {
+		super(id, valor, situacao, servico);
+		this.parcelas = parcelas;
+	}
+
+	public Integer getParcelas() {
+		return parcelas;
+	}
+
+	public void setParcelas(Integer parcelas) {
+		this.parcelas = parcelas;
+	}
+	
+	
+	
+	
+}
